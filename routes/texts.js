b4d70c2eecf85dca6edb797cc96c1e918ddec320
Rafa Gallego <rgdmax@gmail.com>
2018-08-16 20:54:43 +0200

    Added find own text on DB

--- routes/texts.js
+++ routes/texts.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
 const Texto = require('../models/text');
 
 // POST Creating on DB new Text
@@ -15,6 +16,18 @@ router.post('/create', (req, res, next) => {
         res.status(200).json(Texto);
       })
       .catch(next);      
+});
+
+// GET texts listing only logged user texts
+router.get('/', (req, res, next) => {
+  const oid = req.session.currentUser._id;  
+  Texto.find({'owner': ObjectId(oid)})
+      .populate('owner')
+      .then((data) => {
+        console.log(data);
+        res.status(200).json(data);
+      })
+      .catch(next);
 });
 
 module.exports = router;
